ef5a2d547c6c6350fff6e438066339522cb78f06
matinbeigi99 <45142752+matinbeigi99@users.noreply.github.com>
2020-01-07 16:15:59 +0330

    Update config.php

--- config.php
+++ config.php
@@ -2,5 +2,5 @@
 
 define('CLIENT_ID', '77i25k6rf6j8ug');
 define('CLIENT_SECRET', 'szxgYR7CuJhOlwsx');
-define('REDIRECT_URI','https://esgtrade.com/social/callback.php');
+define('REDIRECT_URI','https://your_site.com/callback.php');
 define('SCOPES', 'r_emailaddress,r_liteprofile,w_member_social');
