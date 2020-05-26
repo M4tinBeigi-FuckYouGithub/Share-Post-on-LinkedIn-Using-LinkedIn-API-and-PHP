7de12186ce477ad7b890fd06dfa64760a1f7c26f
Matin Beigi <45142752+matinbeigi99@users.noreply.github.com>
2020-05-26 12:01:41 +0430

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-define('CLIENT_ID', '77i25k6rf6j8ug');
-define('CLIENT_SECRET', 'szxgYR7CuJhOlwsx');
+define('CLIENT_ID', '77i25k6rf68ug');
+define('CLIENT_SECRET', 'szxgYR7CuJhOlsx');
 define('REDIRECT_URI','https://your_site.com/callback.php');
 define('SCOPES', 'r_emailaddress,r_liteprofile,w_member_social');
